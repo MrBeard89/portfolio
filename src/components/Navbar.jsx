a8fa7bb66e970851a9ec165e53b2091bdac1d936
Norbert Kovács <k.n.89@outlook.hu>
2023-03-31 17:46:59 +0200

    Creating files,importing Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import '../styles/Navbar.scss'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar-wrapper'>
+      <div className='navbar-container'>
+        <div className='logo-container'></div>
+      </div>
+    </div>
+  )
+}
