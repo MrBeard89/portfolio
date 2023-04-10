bd631a2423da5051aca68d5a15fa9cb1d4b32bb6
Norbert Kovács <k.n.89@outlook.hu>
2023-04-10 11:54:46 +0200

    Finished Contact hu language

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,28 +1,39 @@
 import '../../styles/Contact/Contact.scss'
 import { Link } from 'react-router-dom'
+import { LanguageContext } from '../../context/LanguageContext'
 
 //Importing Social Icons
 import { FiGithub, FiLinkedin, FiFacebook, FiTwitter } from 'react-icons/fi'
+import { useContext } from 'react'
+//Import Language Library
+const i18n = require('../../utils/i18n.js')
 
 export const Contact = () => {
+  const { language } = useContext(LanguageContext)
+  //
   return (
     <div className='contact-wrapper' id='contact'>
       <div className='contact-container'>
         {/* Email Container */}
         <div className='email-container'>
-          <h3 className='contact-container-title'>Contact</h3>
-          <h2 className='contact-intro-text'>Get in Touch</h2>
+          <h3 className='contact-container-title'>{i18n.text(language, i18n.MAP.contact_title)}</h3>
+          <h2 className='contact-intro-text'>{i18n.text(language, i18n.MAP.contact_intro_text)}</h2>
           <p className='contact-text'>
             {/* Text */}
-            <span className='highlighted'>I'm looking for a Frontend job</span>, so if you
-            interested about me, and you want me to{' '}
-            <span className='highlighted'>join your team</span>, please send me an email !
+            <span className='highlighted'>
+              {i18n.text(language, i18n.MAP.contact_highlighted_text_1)}
+            </span>
+            {i18n.text(language, i18n.MAP.contact_text_1)}{' '}
+            <span className='highlighted'>
+              {i18n.text(language, i18n.MAP.contact_highlighted_text_2)}
+            </span>
+            {i18n.text(language, i18n.MAP.contact_text_2)}
           </p>
 
           {/* Send Email btn */}
           <button className='contact-email-btn'>
             <Link to='mailto:k.n.89@outlook.hu' rel='noopener noreferrer' target='_blank'>
-              Send Email
+              {i18n.text(language, i18n.MAP.contact_email_btn)}
             </Link>
           </button>
         </div>
@@ -56,7 +67,8 @@ export const Contact = () => {
 
           {/* Sign */}
           <p>
-            Designed & built by <span className='full-name'>Norbert Kovács</span>
+            {i18n.text(language, i18n.MAP.contact_footer_text)}
+            <span className='full-name'>{i18n.text(language, i18n.MAP.contact_sign)}</span>
           </p>
         </div>
       </div>
