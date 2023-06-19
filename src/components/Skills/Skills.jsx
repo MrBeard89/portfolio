d56d0c0b9129590ebb1e2237b78f3817cc77d979
Norbert Kovács <k.n.89@outlook.hu>
2023-06-19 15:56:35 +0200

    Added angular to skills,added Me img to index(meta

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -11,7 +11,7 @@ import { AiFillCaretDown } from 'react-icons/ai'
 /////Left
 import Js from '../../assets/tech-icons/js-icon-24.jpg'
 import React from '../../assets/tech-icons/react.png'
-import Css from '../../assets/tech-icons/css3.png'
+import Angular from '../../assets/tech-icons/angular.png'
 import Scss from '../../assets/tech-icons/sass_icon-512.png'
 import Bootstrap from '../../assets/tech-icons/bootstrap.png'
 ////Right
@@ -76,14 +76,18 @@ export const Skills = () => {
 
             <div className='skill-element-container'>
               <AiFillCaretRight className='right-arrow' />
-              <img className='desktop-tech-icon-left' src={Css} alt='tech-icon for desktop' />
-              <li className='brighter'>Css3</li>
+              <img
+                className='desktop-tech-icon-left angular'
+                src={Angular}
+                alt='tech-icon for desktop'
+              />
+              <li className='brighter'>Angular</li>
             </div>
 
             <div className='skill-element-container'>
               <AiFillCaretRight className='right-arrow' />
               <img className='desktop-tech-icon-left' src={Scss} alt='tech-icon for desktop' />
-              <li className='brighter'>Scss</li>
+              <li className='brighter'>Css3,Scss</li>
             </div>
 
             <div className='skill-element-container'>
